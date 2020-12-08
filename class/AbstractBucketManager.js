c59a65990fe4929b97cb0025cdbd122800ec5bd8
Diogo <diogof-6@hotmail.com>
2020-12-08 16:28:08 +0100

    move jsdoc on the good class

--- class/AbstractBucketManager.js
+++ class/AbstractBucketManager.js
@@ -1,19 +1,51 @@
 module.exports = class AbstractBucketManager{
+    /**
+     * @constructor
+     * 
+     * Create an instance with credentials and secret key to acces on the AWS S3
+     */
     constructor() {
       if (this.constructor === AbstractBucketManager) {
         throw new TypeError('Abstract class "AbstractBucketManager" cannot be instantiated directly');
       }
     }
     
+    /**
+     * Create an object on the bucket, create bucket if it not exists
+     * @param {string} objectURL location on bucket (example awsnode.actualit.info/readme.md)
+     * @param {string} filePath  where is the file local or url? (example with local file: c:\just_BI_api\readme.md, example with url: http://perdu.com/)
+     * 
+     * @retun {bool} return false when failed
+     */
     async createObject({objectUrl, filePath = ""}){
       throw new Error('You must implement this function');
     };
+    /**
+     * check the existance of the object / bucket
+     * @param {string} objectUrl location on bucket (example awsnode.actualit.info/readme.md)
+     * @param {string} objectUrl the bucket (example awsnode.actualit.info)
+     * 
+     * @retun {bool} does it exist?
+     */
     async exists({objectUrl}){
       throw new Error('You must implement this function');
     };
+    /**
+     * remove an object on the bucket
+     * @param {string} objectURL location on bucket (example awsnode.actualit.info/readme.md)
+     * 
+     * @retun {bool} return false when failed
+     */
     async removeObject({objectUrl}){
       throw new Error('You must implement this function');
     };
+    /**
+     * download an object of the bucket on a specific location
+     * @param {string} objectUrl location on bucket (example awsnode.actualit.info/readme.md)
+     * @param {string} destinationUri  where is the file has to download (example: c:\just_BI_API\downloaded_files\README.md)
+     * 
+     * @retun {bool} return if it was successful
+     */
     async downloadObject({objectUrl, destinationUri}){
       throw new Error('You must implement this function');
     };
