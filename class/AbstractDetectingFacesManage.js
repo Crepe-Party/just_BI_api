d73839cd7316ec17064e882cc070aee91e337f13
Florian Bergmann <flobergmann@aol.ch>
2020-12-15 15:04:23 +0100

    add abstract detecting face

---
+++ class/AbstractDetectingFacesManage.js
@@ -0,0 +1,24 @@
+module.exports = class AbstractDetectingFacesManage{
+    /**
+     * @constructor
+     * 
+     * Create an instance with credentials and secret key to acces on the AWS S3
+     */
+    constructor() {
+      if (this.constructor === AbstractDetectingFacesManage) {
+        throw new TypeError('Abstract class "AbstractDetectingFacesManage" cannot be instantiated directly');
+      }
+    }
+    
+    /**
+     * Analyse image from local or bucket aws
+     * @param {string} imageUri image local or on a bucket aws
+     * @param {int} maxLabels number of result to return
+     * @param {int} minConfidence % confidence min of result to include of return
+     * 
+     * @retun {Array} return array of result
+     */
+    async makeAnalysisRequest({imageUri, maxLabels = 10, minConfidence = 80}){
+      throw new Error('You must implement this function');
+    };
+}
